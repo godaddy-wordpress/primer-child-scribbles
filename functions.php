6cdb090ee0bbad47a52f76283d524b9aecd96f77
Chris Wallace <chris@liftux.com>
2016-08-17 17:57:56 -0500

    Resetting color schemes.

--- functions.php
+++ functions.php
@@ -211,52 +211,54 @@ function scribbles_font_types( $font_types ) {
 add_filter( 'primer_font_types', 'scribbles_font_types' );
 
 /**
- * Set colors.
+ * Register colors.
  *
- * @filter primer_colors
+ * @action primer_colors
  * @since  1.0.0
- *
- * @param  array $colors
  *
  * @return array
  */
-function scribbles_colors( $colors ) {
+function scribbles_colors() {
 
-	$overrides = array(
-		'header_textcolor' => array(
-			'default' => '#fca903',
-		),
+	return array(
 		'background_color' => array(
+			'label'   => esc_html__( 'Background Color', 'scribbles' ),
 			'default' => '#ffffff',
-		),
-		'header_background_color' => array(
-			'default' => '#ffffff',
-		),
-		'menu_background_color' => array(
-			'default' => '#78ae3e',
-		),
-		'footer_background_color' => array(
-			'default' => '#3787da',
 			'css'     => array(
-				'.footer-widget .widget-title,
-				.footer-widget .widget .search-field' => array(
-					'border-color' => '%1$s',
-				),
-				'.footer-widget .widget-title' => array(
-					'color' => '%1$s',
+				'body' => array(
+					'background-color' => '%1$s',
 				),
 			),
 		),
-		'tagline_text_color' => array(
-			'default' => '#6f6f6f',
+		'header_background_color' => array(
+			'label'   => esc_html__( 'Header Background Color', 'scribbles' ),
+			'default' => '#ffffff',
 			'css'     => array(
-				'.site-search-wrapper .widget .search-field' => array(
+				'.site-header' => array(
+					'background-color' => '%1$s',
+				),
+			),
+		),
+		'header_text_color' => array(
+			'label'   => esc_html__( 'Header Text Color', 'scribbles' ),
+			'default' => '#222222',
+			'css'     => array(
+				'.site-title-wrapper .site-title a,.main-navigation ul a' => array(
 					'color' => '%1$s',
 				),
 			),
 			'rgba_css'     => array(
-				'.site-search-wrapper .widget .search-field' => array(
-					'border-color' => 'rgba(%1$s, 0.25)',
+				'.site-description' => array(
+					'color' => 'rgba(%1$s, 0.6)',
+				),
+			),
+		),
+		'menu_background_color' => array(
+			'label'   => esc_html__( 'Menu Background Color', 'scribbles' ),
+			'default' => '#b5345f',
+			'css'     => array(
+				'.main-navigation-container,.main-navigation ul ul' => array(
+					'background-color' => '%1$s',
 				),
 			),
 		),
@@ -264,38 +266,213 @@ function scribbles_colors( $colors ) {
 			'label'   => esc_html__( 'Menu Text Color', 'scribbles' ),
 			'default' => '#ffffff',
 			'css'     => array(
-				'.main-navigation li a' => array(
+				'.main-navigation ul a, .main-navigation-container .social-menu a' => array(
 					'color' => '%1$s',
 				),
-				'.main-navigation-container:before,
-				.main-navigation-container:after,
-				.menu-toggle div' => array(
+				'.main-navigation .sub-menu .menu-item-has-children > a::after' => array(
+					'border-left-color' => '%1$s',
+				)
+			),
+			'rgba_css'     => array(
+				'.main-navigation ul a, .main-navigation-container .social-menu a' => array(
+					'color' => 'rgba(%1$s, 0.8)',
+				),
+				'.main-navigation .sub-menu .menu-item-has-children > a::after' => array(
+					'border-left-color' => 'rgba(%1$s, 0.8)',
+				)
+			),
+		),
+		'button_color' => array(
+			'label'   => esc_html__( 'Button Color', 'scribbles' ),
+			'default' => '#ffffff',
+			'css'     => array(
+				'button, a.button, a.button:visited, input[type="button"], input[type="reset"], input[type="submit"]' => array(
+					'color' => '%1$s',
+				),
+			),
+			'rgba_css'     => array(
+				'button:hover, a.button:hover, a.button:visited:hover, input[type="button"]:hover, input[type="reset"]:hover, input[type="submit"]:hover' => array(
+					'color' => 'rgba(%1$s, 0.8)',
+				),
+			),
+		),
+		'button_bg_color' => array(
+			'label'   => esc_html__( 'Button Background Color', 'scribbles' ),
+			'default' => '#b5345f',
+			'css'     => array(
+				'button, a.button, a.button:visited, input[type="button"], input[type="reset"], input[type="submit"]' => array(
 					'background-color' => '%1$s',
 				),
 			),
-			'rgba_css' => array(
-				'.main-navigation li a:hover,
-				.main-navigation li a:visited:hover,
-				.main-navigation .current_page_item > a,
-				.main-navigation .current-menu-item > a,
-				.main-navigation .current_page_ancestor > a,
-				.main-navigation .current-menu-ancestor > a' => array(
-					'color' => 'rgba(%1$s, 0.75)',
+			'rgba_css'     => array(
+				'button:hover, a.button:hover, a.button:visited:hover, input[type="button"]:hover, input[type="reset"]:hover, input[type="submit"]:hover' => array(
+					'background-color' => 'rgba(%1$s, 0.8)',
+				),
+			),
+		),
+		'footer_social_color' => array(
+			'label'   => esc_html__( 'Footer Social Icon Color', 'scribbles' ),
+			'default' => '#b5345f',
+			'css'     => array(
+				'.site-info-wrapper a, .site-info .social-menu a, .social-menu a' => array(
+					'color' => '%1$s',
+				),
+			),
+		),
+		'footer_background_color' => array(
+			'label'   => esc_html__( 'Footer Background Color', 'scribbles' ),
+			'default' => '#ffffff',
+			'css'     => array(
+				'.site-info-wrapper, .footer-nav, .site-info-wrapper' => array(
+					'background-color' => '%1$s',
 				),
 			),
 		),
 		'link_color' => array(
-			'default' => '#3787da',
+			'label'   => esc_html__( 'Link Color', 'scribbles' ),
+			'default' => '#54ccbe',
+			'css'     => array(
+				'a, a:visited, .entry-footer a, .sticky .entry-title a:before, .footer-widget-area .footer-widget a, .main-navigation-container .menu li.current-menu-item > a:hover' => array(
+					'color' => '%1$s',
+				),
+			),
 		),
-		'main_text_color' => array(
-			'default' => '#404040',
+		'w_background_color' => array(
+			'label'   => esc_html__( 'Footer Widget Background Color', 'scribbles' ),
+			'default' => '#3f3244',
+			'css'     => array(
+				'.site-footer' => array(
+					'background-color' => '%1$s',
+				),
+			),
 		),
-		'secondary_text_color' => array(
-			'default' => '#686868',
+		'w_text_color' => array(
+			'label'   => esc_html__( 'Footer Widget Text Color', 'scribbles' ),
+			'default' => '#ffffff',
+			'css'     => array(
+				'.site-footer-inner' => array(
+					'color' => '%1$s',
+				),
+			),
 		),
 	);
 
-	return primer_array_replace_recursive( $colors, $overrides );
+}
+add_action( 'primer_colors', 'scribbles_colors' );
+
+/**
+ * Register color schemes.
+ *
+ * @action primer_color_schemes
+ * @since  1.0.0
+ *
+ * @return array
+ */
+function scribbles_color_schemes() {
+
+	return array(
+		'bronze' => array(
+			'label'  => esc_html__( 'Bronze', 'scribbles' ),
+			'colors' => array(
+				'background_color'        => '#ffffff',
+				'header_background_color' => '#ffffff',
+				'header_text_color'       => '#222222',
+				'menu_background_color'   => '#b1a18b',
+				'menu_text_color'         => '#ffffff',
+				'button_color'            => '#ffffff',
+				'button_bg_color'         => '#b1a18b',
+				'footer_social_color'     => '#b1a18b',
+				'footer_background_color' => '#ffffff',
+				'link_color'              => '#b1a18b',
+				'w_background_color'      => '#b1a18b',
+				'w_text_color'            => '#ffffff',
+			),
+		),
+		'red' => array(
+			'label'  => esc_html__( 'Red', 'scribbles' ),
+			'colors' => array(
+				'background_color'        => '#ffffff',
+				'header_background_color' => '#ffffff',
+				'header_text_color'       => '#222222',
+				'menu_background_color'   => '#cc494f',
+				'button_color'            => '#ffffff',
+				'button_bg_color'         => '#cc494f',
+				'footer_social_color'     => '#cc494f',
+				'footer_background_color' => '#ffffff',
+				'link_color'              => '#cc494f',
+				'w_background_color'      => '#cc494f',
+				'w_text_color'            => '#ffffff',
+			),
+		),
+		'blue' => array(
+			'label'  => esc_html__( 'Blue', 'scribbles' ),
+			'colors' => array(
+				'background_color'        => '#ffffff',
+				'header_background_color' => '#ffffff',
+				'header_text_color'       => '#222222',
+				'menu_background_color'   => '#499ccc',
+				'menu_text_color'         => '#ffffff',
+				'button_color'            => '#ffffff',
+				'button_bg_color'         => '#499ccc',
+				'footer_social_color'     => '#499ccc',
+				'footer_background_color' => '#ffffff',
+				'link_color'              => '#499ccc',
+				'w_background_color'      => '#d6ebf9',
+				'w_text_color'            => '#636363',
+			),
+		),
+		'green' => array(
+			'label'  => esc_html__( 'Green', 'scribbles' ),
+			'colors' => array(
+				'background_color'        => '#ffffff',
+				'header_background_color' => '#ffffff',
+				'header_text_color'       => '#222222',
+				'menu_background_color'   => '#62bf7c',
+				'menu_text_color'         => '#ffffff',
+				'button_color'            => '#ffffff',
+				'button_bg_color'         => '#62bf7c',
+				'footer_social_color'     => '#62bf7c',
+				'footer_background_color' => '#ffffff',
+				'link_color'              => '#62bf7c',
+				'w_background_color'      => '#f2f2f2',
+				'w_text_color'            => '#888888',
+			),
+		),
+		'orange' => array(
+			'label'  => esc_html__( 'Orange', 'scribbles' ),
+			'colors' => array(
+				'background_color'        => '#ffffff',
+				'header_background_color' => '#ffffff',
+				'header_text_color'       => '#222222',
+				'menu_background_color'   => '#df6135',
+				'menu_text_color'         => '#ffffff',
+				'button_color'            => '#ffffff',
+				'button_bg_color'         => '#df6135',
+				'footer_social_color'     => '#df6135',
+				'footer_background_color' => '#ffffff',
+				'link_color'              => '#df6135',
+				'w_background_color'      => '#222222',
+				'w_text_color'            => '#ffffff',
+			),
+		),
+		'yellow' => array(
+			'label'  => esc_html__( 'Yellow', 'scribbles' ),
+			'colors' => array(
+				'background_color'        => '#e9e73d',
+				'header_background_color' => '#e9e73d',
+				'header_text_color'       => '#222222',
+				'menu_background_color'   => '#222222',
+				'menu_text_color'         => '#ffffff',
+				'button_color'            => '#222222',
+				'button_bg_color'         => '#e9e73d',
+				'footer_social_color'     => '#222222',
+				'footer_background_color' => '#e9e73d',
+				'link_color'              => '#777777',
+				'w_background_color'      => '#e9e73d',
+				'w_text_color'            => '#3a3a3a',
+			),
+		),
+	);
 
 }
-add_filter( 'primer_colors', 'scribbles_colors' );
+add_action( 'primer_color_schemes', 'scribbles_color_schemes' );
